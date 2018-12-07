e5a59dc4b66cbc819fe7eb999d520a718ea7adc4
Victor <jucianovictor@yahoo.com.br>
2018-12-07 00:29:27 -0200

    Colocando a tela de Upsert(Cadastrar e alterar)  e chamando ela no menu

--- src/DAO/CarroDAO.java
+++ src/DAO/CarroDAO.java
@@ -1,7 +1,6 @@
 package DAO;
 
 import model.Carro;
-import model.Usuario;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,34 +8,52 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import java.sql.Connection;
+import javax.swing.JOptionPane;
 
 public class CarroDAO extends ExecuteSQL {
 
-	public CarroDAO(Connection con) {
-		super(con);
+    public CarroDAO(Connection con) {
+        super(con);
 
-	}
+    }
 
-	public ArrayList<Carro> getCarroLista() {
-		ArrayList<Carro> carros = new ArrayList<>();
-		String query = "Select * From Carro";
-		try {
-			PreparedStatement ps = getCon().prepareStatement(query);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				Carro carro = new Carro();
-				carro.setNome(rs.getString("NOME"));
-				carro.setAno(rs.getString("ANO"));
-				carro.setFabricante(rs.getString("FABRICANTE"));
-				carro.setPreco(rs.getFloat("PRECO"));
-				carros.add(carro);
-			}
-		} catch (SQLException ex) {
-			System.err.println(ex.getMessage());
-		}
+    public ArrayList<Carro> getCarroLista() {
+        ArrayList<Carro> carros = new ArrayList<>();
+        String query = "Select * From Carro";
+        try {
+            PreparedStatement ps = getCon().prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Carro carro = new Carro();
+                carro.setNome(rs.getString("NOME"));
+                carro.setAno(rs.getString("ANO"));
+                carro.setFabricante(rs.getString("FABRICANTE"));
+                carro.setPreco(rs.getFloat("PRECO"));
+                carros.add(carro);
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
 
-		return carros;
+        return carros;
+    }
 
-	}
+    public boolean AlterarCarro(int pk_ve, String newNome, String newAno, String newFabricante, float newPreco) {
 
+        boolean isUser = false;
+
+        String consulta = "UPDATE Carros SET nome = '" + newNome + "' , ano = '" + newAno + "', fabricante = '" + newFabricante + "', preco = '" + newPreco + "'  WHERE pk_ve = '" + pk_ve + "'";
+
+        try {
+            PreparedStatement ps = getCon().prepareStatement(consulta);
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Alterado com sucesso");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao logar");
+            System.out.println("CAUSADO POR: " + e.getMessage());
+        }
+        Conexao.FecharConexão(getCon());
+        return isUser;
+    }
 }
--- src/control/MenuController.java
+++ src/control/MenuController.java
@@ -5,9 +5,18 @@
  */
 package control;
 
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
@@ -15,9 +24,43 @@ import javafx.fxml.Initializable;
  */
 public class MenuController implements Initializable {
 
+    @FXML
+    private JFXButton logoutBTN;
+
+    @FXML
+    private JFXButton cadastroBTN;
+
+    @FXML
+    private JFXButton listarBTN;
+
+    @FXML
+    void handlerGoCadastrar(ActionEvent event) throws IOException {
+        Parent menu_parent = FXMLLoader.load(getClass().getResource("/view/UpsertFXML.fxml"));
+        Scene menu_scene = new Scene(menu_parent);
+
+        //Setando o palco a partir do palco anterior
+        Stage palco = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        palco.setScene(menu_scene);
+    }
+
+    @FXML
+    void logout(ActionEvent event) throws IOException {
+        Parent menu_parent = FXMLLoader.load(getClass().getResource("/view/LoginFXML.fxml"));
+        Scene menu_scene = new Scene(menu_parent);
+
+        //Setando o palco a partir do palco anterior
+        Stage palco = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        palco.setScene(menu_scene);
+    }
+
+    @FXML
+    void handlerGoListar(ActionEvent event) throws IOException {
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+
     }
-    
+
 }
