58a1abe7f3ccfc9ddae69bc37bd451407440776f
Victor <jucianovictor@yahoo.com.br>
2018-12-10 23:42:43 -0200

    Otimização no CarroDAO, colocando id no modelo de carro e  Upsesert

--- src/DAO/CarroDAO.java
+++ src/DAO/CarroDAO.java
@@ -14,7 +14,6 @@ public class CarroDAO extends ExecuteSQL {
 
     public CarroDAO(Connection con) {
         super(con);
-
     }
 
     public ArrayList<Carro> getCarroLista() {
@@ -34,26 +33,58 @@ public class CarroDAO extends ExecuteSQL {
         } catch (SQLException ex) {
             System.err.println(ex.getMessage());
         }
-
+        Conexao.FecharConexão(getCon());
         return carros;
     }
 
     public boolean AlterarCarro(int pk_ve, String newNome, String newAno, String newFabricante, float newPreco) {
-
-        boolean isUser = false;
-
-        String consulta = "UPDATE Carros SET nome = '" + newNome + "' , ano = '" + newAno + "', fabricante = '" + newFabricante + "', preco = '" + newPreco + "'  WHERE pk_ve = '" + pk_ve + "'";
-
+            // criando o codigo sql para trabalhar com o banco
+        String consulta
+                = "UPDATE carro SET "
+                + "nome = '" + newNome + "', "
+                + " ano = '" + newAno + "', "
+                + " fabricante = '" + newFabricante + "', "
+                + "preco = '" + newPreco + "'"
+                + "  WHERE pk_niv = '" + pk_ve + "'";
         try {
-            PreparedStatement ps = getCon().prepareStatement(consulta);
-            ps.executeUpdate();
-            JOptionPane.showMessageDialog(null, "Alterado com sucesso");
-
+            PreparedStatement ps = getCon().prepareStatement(consulta);           
+            //Realizando um teste para ver se a Execução da consulta no banco deu certo, ele retorna 1 se der certo
+            if (ps.executeUpdate()>0) {
+                Conexao.FecharConexão(getCon());
+                return true;
+            }
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Erro ao logar");
+            JOptionPane.showMessageDialog(null, "Erro ao Alterar");
             System.out.println("CAUSADO POR: " + e.getMessage());
         }
         Conexao.FecharConexão(getCon());
-        return isUser;
+        return false;
+    }
+    
+        public boolean CadastrarCarro(String nomeCarro, String ano, String fabricante, float preco) {
+            // criando o codigo sql para trabalhar com o banco
+        String consulta
+                = "INSERT INTO carro "
+                + "("
+                + "pk_niv,"
+                + " nome, "
+                + "ano, "
+                + "fabricante,"
+                + " preco"
+                + ") " 
+                + "SELECT COUNT(pk_niv) +1 , '"+nomeCarro+"', '"+ano+"', '"+fabricante+"', "+preco+" FROM carro";
+        try {
+            PreparedStatement ps = getCon().prepareStatement(consulta);
+             //Realizando um teste para ver se a Execução da consulta no banco deu certo, ele retorna 1 se der certo
+            if (ps.executeUpdate()>0) {
+                Conexao.FecharConexão(getCon());
+                return true;
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao Cadastrar");
+            System.out.println("CAUSADO POR: " + e.getMessage());
+        }
+        Conexao.FecharConexão(getCon());
+        return false;
     }
 }
